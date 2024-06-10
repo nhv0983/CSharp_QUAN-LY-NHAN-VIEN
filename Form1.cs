05a47fd392d3c4d0a59a2de9851c6efc21433bc2
ADMIN <ADMIN@LAB1-MAY23>
2024-06-10 14:41:35 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_QUAN_LY_NHAN_VIEN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lsbten.Items.Add(txtSo.Text);
+        }
+    }
+}
